e4298417b4b21a7e12c2d57366612e4802f59fd3
Aidan Henning <henningaidan@gmail.com>
2026-01-23 14:46:38 -0800

    added content and styling to entry page

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Link to="/login" className={styles.btnPrimary}>
+        Sign Up
+      </Link>
+    </header>
+  );
+}
--- frontend/src/pages/Entry/Entry.jsx
+++ frontend/src/pages/Entry/Entry.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import styles from "./Entry.module.css";
+import { Link } from "react-router";
 
 export default function Entry() {
-  return <div>Entry</div>;
+  return (
+    <>
+      <div className={styles.entryBackgroundDiv}>
+        <div className={styles.entryBackgroundFade}></div>
+      </div>
+      <div className={styles.entryContent}>
+        <div className={styles.entryText}>
+          <h1 className={styles.stackedHeading}>
+            <span>Community</span>
+            <span>Driven</span>
+            <div className={styles.highlight}></div>
+          </h1>
+          <h2 className={styles.subheading}>Five @ Six</h2>
+        </div>
+        <Link to="/login" className={styles.btnSecondary}>
+          Get Started
+        </Link>
+      </div>
+    </>
+  );
 }
