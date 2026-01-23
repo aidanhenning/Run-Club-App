cc7725325921daa9465c72b9b1641fe29db482ac
Aidan Henning <henningaidan@gmail.com>
2026-01-23 11:55:54 -0800

    created entry, login, register, and home pages

---
+++ frontend/src/pages/Entry/Entry.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Entry.module.css";
+
+export default function Entry() {
+  return <div>Entry</div>;
+}
---
+++ frontend/src/pages/Register/Register.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Register.module.css";
+
+export default function Register() {
+  return <div>Register</div>;
+}
