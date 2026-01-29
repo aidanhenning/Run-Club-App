97c144a39295288da3381f773f197e03c2ebfb7e
Aidan Henning <henningaidan@gmail.com>
2026-01-28 17:41:10 -0800

    created search, club, and profile pages with connected css files

---
+++ frontend/src/pages/Clubs/Clubs.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Clubs.module.css";
+
+export default function Clubs() {
+  return <div>Clubs</div>;
+}
---
+++ frontend/src/pages/Search/Search.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Search.module.css";
+
+export default function Search() {
+  return <div>Search</div>;
+}
