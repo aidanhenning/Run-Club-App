2eabbbc1f998c1b5d132ba68255fec4b071d5817
Aidan Henning <henningaidan@gmail.com>
2026-02-11 17:48:36 -0800

    updated create and update functions for posts to match new schema

--- backend/db/queries/posts.js
+++ backend/db/queries/posts.js
@@ -2,32 +2,36 @@ import db from "../client.js";
 
 export async function createPost({
   clubId,
-  hostId,
   title,
   startsAt,
-  typeOfRun,
-  distance,
-  terrain,
   address,
+  distance,
+  elevation,
+  runType,
+  estimatedTime,
+  bibleReference,
+  bibleText,
 }) {
   const sql = `
   INSERT INTO posts
-    (club_id, host_id, title, starts_at, type_of_run, distance, terrain, address)
+    (club_id, title, starts_at, address, distance, elevation, run_type, estimated_time, bible_reference, bible_text)
   VALUES
-    ($1, $2, $3, $4, $5, $6, $7, $8) 
+    ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) 
   RETURNING *
   `;
   const {
     rows: [post],
   } = await db.query(sql, [
     clubId,
-    hostId,
     title,
     startsAt,
-    typeOfRun,
-    distance,
-    terrain,
     address,
+    distance,
+    elevation,
+    runType,
+    estimatedTime,
+    bibleReference,
+    bibleText,
   ]);
   return post;
 }
@@ -55,14 +59,17 @@ export async function updatePostById(
   postId,
   title,
   startsAt,
-  typeOfRun,
-  distance,
-  terrain,
   address,
+  distance,
+  elevation,
+  runType,
+  estimatedTime,
+  bibleReference,
+  bibleText,
 ) {
   const sql = `
   UPDATE posts 
-  SET title = $2, starts_at = $3, type_of_run = $4, distance = $5, terrain = $6, address = $7
+  SET title = $2, starts_at = $3, address = $4, distance = $5, elevation = $6, run_type = $7, estimated_time = $8, bible_reference = $9, bible_text = $10
   WHERE id = $1 
   RETURNING *
   `;
@@ -72,10 +79,13 @@ export async function updatePostById(
     postId,
     title,
     startsAt,
-    typeOfRun,
-    distance,
-    terrain,
     address,
+    distance,
+    elevation,
+    runType,
+    estimatedTime,
+    bibleReference,
+    bibleText,
   ]);
   return club;
 }
