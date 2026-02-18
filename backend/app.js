0c896d5255e2c89058d2f81dfb32a7c7474a89aa
Aidan Henning <henningaidan@gmail.com>
2026-02-17 16:24:49 -0800

    updated app.js to include all new routers

--- backend/app.js
+++ backend/app.js
@@ -8,17 +8,37 @@ import clubMembershipsRouter from "./routes/club_memberships.js";
 import followersRouter from "./routes/followers.js";
 import feedRouter from "./routes/feed.js";
 import searchRouter from "./routes/search.js";
+import postsRouter from "./routes/posts.js";
+import postPicturesRouter from "./routes/postPictures.js";
+import postAttendeesRouter from "./routes/postAttendees.js";
+import postCommentsRouter from "./routes/postComments.js";
+import postLikesRouter from "./routes/postLikes.js";
+import commentLikesRouter from "./routes/commentLikes.js";
+
 import getUserFromToken from "./middleware/getUserFromToken.js";
 
 app.use(express.json());
 app.use(getUserFromToken);
 
+// Base Resources
 app.use("/users", usersRouter);
 app.use("/clubs", clubsRouter);
 app.use("/clubmemberships", clubMembershipsRouter);
 app.use("/followers", followersRouter);
-app.use("/feed", feedRouter);
 app.use("/search", searchRouter);
+
+// Feed & Posts
+app.use("/feed", feedRouter);
+app.use("/posts", postsRouter);
+
+// Post Interactions
+app.use("/post-pictures", postPicturesRouter);
+app.use("/post-attendees", postAttendeesRouter);
+app.use("/post-comments", postCommentsRouter);
+app.use("/post-likes", postLikesRouter);
+
+// Comment Interactions
+app.use("/comment-likes", commentLikesRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the runclub API!");
