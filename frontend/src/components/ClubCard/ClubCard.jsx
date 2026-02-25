c6399fd6a7381882e50c5b83930c89366d700ad2
Aidan Henning <henningaidan@gmail.com>
2026-02-24 16:03:45 -0800

    added link to ClubCard component so users can navigate to club profile page

--- frontend/src/components/ClubCard/ClubCard.jsx
+++ frontend/src/components/ClubCard/ClubCard.jsx
@@ -1,23 +1,26 @@
 import styles from "./ClubCard.module.css";
+import { Link } from "react-router";
 import { MdFlag } from "react-icons/md";
 
 export default function ClubCard({ club }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.clubLogo}>
-        {club.logo === null ? (
-          <MdFlag className={styles.placeholder} />
-        ) : (
-          club.logo
-        )}
-      </div>
-      <div className={styles.clubText}>
-        <div className={styles.clubName}>{club.name}</div>
-        <div className={styles.clubMemberCount}>
-          {club.member_count} members
+    <Link to={`/clubs/${club.id}`} className={styles.cardLink}>
+      <div className={styles.card}>
+        <div className={styles.clubLogo}>
+          {club.logo === null ? (
+            <MdFlag className={styles.placeholder} />
+          ) : (
+            club.logo
+          )}
         </div>
-        <div className={styles.cta}>click to see details</div>
+        <div className={styles.clubText}>
+          <div className={styles.clubName}>{club.name}</div>
+          <div className={styles.clubMemberCount}>
+            {club.member_count} members
+          </div>
+          <div className={styles.cta}>click to see details</div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
