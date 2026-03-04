4ecbd1c035b0651a10ccd638a12c1d8b13f1b614
Aidan Henning <henningaidan@gmail.com>
2026-03-04 14:40:36 -0800

    create link to create-club on the ClubList page

--- frontend/src/components/Pages/ClubsList/CreateClubBanner/CreateClubBanner.jsx
+++ frontend/src/components/Pages/ClubsList/CreateClubBanner/CreateClubBanner.jsx
@@ -1,10 +1,14 @@
 import styles from "@/components/Pages/ClubsList/CreateClubBanner/CreateClubBanner.module.css";
 
+import { Link } from "react-router";
+
 export default function CreateClubBanner() {
   return (
     <div className={styles.createClubBanner}>
       <p className={styles.createClubText}>Create your own club</p>
-      <button className={styles.createClubButton}>Create a Club</button>
+      <Link to="/create-club" className={styles.createClubButton}>
+        Create a Club
+      </Link>
     </div>
   );
 }
