9a0a7b9f13dcc1416091d47e1ebefb4454b90675
Aidan Henning <henningaidan@gmail.com>
2026-03-06 16:15:24 -0800

    completed Details component in PostDetails page

--- frontend/src/components/Pages/PostDetails/Details/Details.jsx
+++ frontend/src/components/Pages/PostDetails/Details/Details.jsx
@@ -1,5 +1,99 @@
 import styles from "@/components/Pages/PostDetails/Details/Details.module.css";
 
-export default function Details() {
-  return <div>Details</div>;
+import { useNavigate } from "react-router";
+import { MdFlag, MdLocationOn } from "react-icons/md";
+import { FaCrown } from "react-icons/fa";
+
+export default function Details({ details }) {
+  const navigate = useNavigate();
+
+  const formatPostDateDay = (dateString) => {
+    const date = new Date(dateString);
+
+    const datePart = date.toLocaleDateString("en-US", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    });
+
+    return datePart;
+  };
+
+  const formatPostDateTime = (dateString) => {
+    const date = new Date(dateString);
+
+    const timePart = date.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+    return timePart;
+  };
+
+  return (
+    <section className={styles.details}>
+      <div>
+        <h2 className={styles.title}>{details.title}</h2>
+      </div>
+      {details?.club_logo ? (
+        <img
+          src={details.club_logo}
+          alt={`${details.club_name} logo`}
+          className={styles.clubLogo}
+        />
+      ) : (
+        <MdFlag className={styles.placeholder} />
+      )}
+      <div className={styles.date}>
+        <span className={styles.day}>
+          {formatPostDateDay(details.starts_at)}
+        </span>
+        <span className={styles.time}>
+          {formatPostDateTime(details.starts_at)}
+        </span>
+      </div>
+      <div className={styles.stats}>
+        <div className={styles.statsItem}>
+          <span className={styles.label}>Distance</span>
+          <span className={styles.value}>{details.distance}mi</span>
+        </div>
+        <div className={styles.statsItem}>
+          <span className={styles.label}>Elev</span>
+          <span className={styles.value}>{details.elevation}ft</span>
+        </div>
+        <div className={styles.statsItem}>
+          <span className={styles.label}>Type</span>
+          <span className={styles.value}>{details.run_type}</span>
+        </div>
+        <div className={styles.statsItem}>
+          <span className={styles.label}>Time</span>
+          <span className={styles.value}>{details.estimated_time}</span>
+        </div>
+      </div>
+      <div className={styles.host}>
+        <h3 className={styles.hostTitle}>
+          <span className={styles.icon}>
+            <FaCrown />
+          </span>
+          <span>Hosted by</span>
+        </h3>
+        <img
+          src={details.owner_profile_picture}
+          alt={`${details.owner_first_name} ${details.owner_last_name}'s profile picture`}
+          onClick={() => navigate(`/profile/${details.club_owner_id}`)}
+          className={styles.profilePicture}
+        />
+      </div>
+      <div>
+        <p className={styles.location}>
+          <span className={styles.icon}>
+            <MdLocationOn />
+          </span>
+          <span>{details.address}</span>
+        </p>
+      </div>
+      {/* <section>{details.description}</section> */}
+    </section>
+  );
 }
--- frontend/src/pages/PostDetails/PostDetails.jsx
+++ frontend/src/pages/PostDetails/PostDetails.jsx
@@ -1,4 +1,5 @@
-import styles from "@/pages/PostDetails/PostDetails";
+import styles from "@/pages/PostDetails/PostDetails.module.css";
+import { useAuth } from "@/context/AuthContext";
 import Header from "@/components/Header/Header";
 import BottomNav from "@/components/BottomNav/BottomNav";
 import Details from "@/components/Pages/PostDetails/Details/Details";
@@ -6,15 +7,52 @@ import RsvpList from "@/components/Pages/PostDetails/RsvpList/RsvpList";
 import PhotoAlbum from "@/components/Pages/PostDetails/PhotoAlbum/PhotoAlbum";
 import Comments from "@/components/Pages/PostDetails/Comments/Comments";
 
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+
 export default function PostDetails() {
+  const { API, token } = useAuth();
+  const { id } = useParams();
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [post, setPost] = useState([]);
+
+  console.log(post);
+
+  const fetchPostDetails = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(`${API}/posts/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      setPost(data);
+    } catch (err) {
+      console.error("Failed to fetch feed:", err);
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!token || !API) return;
+    fetchPostDetails(0, true);
+  }, [token, API]);
+
   return (
     <div className={styles.container}>
       <Header title="" showBack={true} />
 
-      <Details />
-      <RsvpList />
-      <PhotoAlbum />
-      <Comments />
+      <div className={styles.content}>
+        <Details details={post} />
+        <RsvpList attendees={post.attendees} />
+        <PhotoAlbum pictures={post.pictures} />
+        <Comments comments={post.comments} />
+      </div>
 
       <BottomNav />
     </div>
