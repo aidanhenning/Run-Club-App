963927ddab4d992a4337d474da8c115e013c034e
Aidan Henning <henningaidan@gmail.com>
2026-03-09 09:42:43 -0700

    added expanded view to PictureCard component

--- frontend/src/components/Pages/UserProfile/PictureCard/PictureCard.jsx
+++ frontend/src/components/Pages/UserProfile/PictureCard/PictureCard.jsx
@@ -1,11 +1,37 @@
 import styles from "@/components/Pages/UserProfile/PictureCard/PictureCard.module.css";
 
+import { useState } from "react";
+import { IoCloseCircle } from "react-icons/io5";
+
 export default function PictureCard({ picture }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <img
-      src={picture.image_url}
-      alt="user uploaded image"
-      className={styles.picture}
-    />
+    <>
+      <div className={styles.wrapper} onClick={() => setIsExpanded(true)}>
+        <img
+          src={picture.image_url}
+          alt="user uploaded content"
+          className={styles.picture}
+        />
+      </div>
+
+      {isExpanded && (
+        <div className={styles.overlay} onClick={() => setIsExpanded(false)}>
+          <button
+            className={styles.closeBtn}
+            onClick={() => setIsExpanded(false)}
+          >
+            <IoCloseCircle />
+          </button>
+          <img
+            src={picture.image_url}
+            alt="Expanded view"
+            className={styles.expandedImage}
+            onClick={(e) => e.stopPropagation()}
+          />
+        </div>
+      )}
+    </>
   );
 }
